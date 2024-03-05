1357f72d6812de31b0c9633501d0f6df0f5a0bff
LociStar <locistar.got@gmail.com>
2024-03-05 13:04:42 +0100

    fixed uploadDeck

--- src/main/java/com/loci/ato_deck_builder_server/api/deck/DeckHandler.java
+++ src/main/java/com/loci/ato_deck_builder_server/api/deck/DeckHandler.java
@@ -14,13 +14,15 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Component
 public class DeckHandler {
-    private final DeckRepository charRepository;
+    private final DeckRepository deckRepository;
     private final DeckCardRepository deckCardRepository;
 
-    public DeckHandler(DeckRepository charRepository, DeckCardRepository deckCardRepository) {
-        this.charRepository = charRepository;
+    public DeckHandler(DeckRepository deckRepository, DeckCardRepository deckCardRepository) {
+        this.deckRepository = deckRepository;
         this.deckCardRepository = deckCardRepository;
     }
 
@@ -29,17 +31,20 @@ public class DeckHandler {
         int size = Integer.parseInt(request.queryParam("size").orElse("10"));
         String searchQuery = "%" + request.queryParam("searchQuery").orElse("") + "%";
         Pageable pageable = PageRequest.of(page, size);
-        Flux<Deck> decks = charRepository.findByTitleContaining(searchQuery, pageable.getPageSize(), pageable.getOffset());
+        Flux<Deck> decks = deckRepository.findByTitleContaining(searchQuery, pageable.getPageSize(), pageable.getOffset());
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(decks.collectList(), Deck.class);
     }
 
     public Mono<ServerResponse> uploadDeck(ServerRequest request) {
         return request.bodyToMono(WebDeck.class)
                 .flatMap(webDeck -> {
-                    Mono<Deck> savedDeck = charRepository.save(webDeck.toDeck());
-                    Flux<DeckCard> savedCards = deckCardRepository.saveAll(webDeck.getCards());
-                    return Mono.zip(savedDeck, savedCards.collectList());
-                })
-                .flatMap(tuple -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(tuple.getT1()));
+                    Mono<Integer> savedDeck = deckRepository.insertDeck(webDeck.getTitle(), webDeck.getDescription(), webDeck.getCharacterId(), "123");
+
+                    return savedDeck.map(id -> {
+                        List<DeckCard> cards = webDeck.toDeckCards(id);
+                        return deckCardRepository.saveAll(cards);
+                    });
+                }).flatMap(Flux::collectList)
+                .then(ServerResponse.ok().build());
     }
 }
