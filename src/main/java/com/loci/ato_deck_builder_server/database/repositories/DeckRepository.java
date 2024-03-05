23399309b5a0b68a73b79c06fa4855baa1c4492e
LociStar <locistar.got@gmail.com>
2024-03-05 13:04:55 +0100

    added insertDeck

--- src/main/java/com/loci/ato_deck_builder_server/database/repositories/DeckRepository.java
+++ src/main/java/com/loci/ato_deck_builder_server/database/repositories/DeckRepository.java
@@ -4,8 +4,9 @@ import com.loci.ato_deck_builder_server.database.objects.Deck;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-public interface DeckRepository extends R2dbcRepository<Deck, String> {
+public interface DeckRepository extends R2dbcRepository<Deck, Integer> {
     @Query("""
             SELECT * FROM deck
             WHERE title ILIKE $1
@@ -13,4 +14,11 @@ public interface DeckRepository extends R2dbcRepository<Deck, String> {
             LIMIT $2 OFFSET $3
             """)
     Flux<Deck> findByTitleContaining(String name, int limit, long offset);
+
+    @Query("""
+            INSERT INTO deck (title, description, char_id, user_id)
+            VALUES ($1, $2, $3, $4)
+            RETURNING deck_id
+            """)
+    Mono<Integer> insertDeck(String title, String description, String characterId, String userId);
 }
