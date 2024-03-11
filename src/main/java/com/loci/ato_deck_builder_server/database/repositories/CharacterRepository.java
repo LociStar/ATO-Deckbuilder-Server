2866d20f2240981964922ada1592faf0e54daa5d
LociStar <locistar.got@gmail.com>
2024-03-11 12:57:27 +0100

    added import

--- src/main/java/com/loci/ato_deck_builder_server/database/repositories/CharacterRepository.java
+++ src/main/java/com/loci/ato_deck_builder_server/database/repositories/CharacterRepository.java
@@ -1,5 +1,6 @@
 package com.loci.ato_deck_builder_server.database.repositories;
 
+import com.loci.ato_deck_builder_server.database.objects.Character;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
 public interface CharacterRepository extends R2dbcRepository<Character, String>{
