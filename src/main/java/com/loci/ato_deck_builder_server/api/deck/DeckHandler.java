074fbf8c0624aaebc5673bdc248aa3ab1a9e93d0
LociStar <locistar.got@gmail.com>
2024-03-11 12:58:43 +0100

    modified uploadDeck added variables to getDecks (for future search improvement)

--- src/main/java/com/loci/ato_deck_builder_server/api/deck/DeckHandler.java
+++ src/main/java/com/loci/ato_deck_builder_server/api/deck/DeckHandler.java
@@ -8,6 +8,9 @@ import com.loci.ato_deck_builder_server.database.repositories.DeckRepository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -30,15 +33,19 @@ public class DeckHandler {
         int page = Integer.parseInt(request.queryParam("page").orElse("0"));
         int size = Integer.parseInt(request.queryParam("size").orElse("10"));
         String searchQuery = "%" + request.queryParam("searchQuery").orElse("") + "%";
+        int upgradeCostReduction = Integer.parseInt(request.queryParam("upgradeCostReduction").orElse("0"));
+        int craftingCostReduction = Integer.parseInt(request.queryParam("craftingCostReduction").orElse("0"));
         Pageable pageable = PageRequest.of(page, size);
         Flux<Deck> decks = deckRepository.findByTitleContaining(searchQuery, pageable.getPageSize(), pageable.getOffset());
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(decks.collectList(), Deck.class);
     }
 
     public Mono<ServerResponse> uploadDeck(ServerRequest request) {
+        Mono<Authentication> authenticationMono = ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication);
         return request.bodyToMono(WebDeck.class)
                 .flatMap(webDeck -> {
-                    Mono<Integer> savedDeck = deckRepository.insertDeck(webDeck.getTitle(), webDeck.getDescription(), webDeck.getCharacterId(), "123");
+                    Mono<Integer> savedDeck = authenticationMono.flatMap(authentication -> deckRepository.insertDeck(webDeck.getTitle(), webDeck.getDescription(), webDeck.getCharacterId(), authentication.getName()));
 
                     return savedDeck.map(id -> {
                         List<DeckCard> cards = webDeck.toDeckCards(id);
