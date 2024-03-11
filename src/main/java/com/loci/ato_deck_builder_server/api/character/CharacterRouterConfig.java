6e17bd0b9bef69c5caf045b2c098efab4a46b378
LociStar <locistar.got@gmail.com>
2024-03-11 12:57:51 +0100

    added CharacterRouterConfig

---
+++ src/main/java/com/loci/ato_deck_builder_server/api/character/CharacterRouterConfig.java
@@ -0,0 +1,18 @@
+package com.loci.ato_deck_builder_server.api.character;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration(proxyBeanMethods = false)
+public class CharacterRouterConfig {
+
+    @Bean
+    protected RouterFunction<ServerResponse> characterRoutes(CharacterHandler characterHandler) {
+        return RouterFunctions.route()
+                .GET("/character", characterHandler::getAllCharacters)
+                .build();
+    }
+}
