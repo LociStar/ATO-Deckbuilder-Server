714f4e13de06bd862bb92f5a417c7effa56ffce3
LociStar <locistar.got@gmail.com>
2024-03-16 11:41:17 +0100

    implemented findByDeckId query

--- src/main/java/com/loci/ato_deck_builder_server/database/repositories/DeckCardRepository.java
+++ src/main/java/com/loci/ato_deck_builder_server/database/repositories/DeckCardRepository.java
@@ -1,7 +1,11 @@
 package com.loci.ato_deck_builder_server.database.repositories;
 
 import com.loci.ato_deck_builder_server.database.objects.DeckCard;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Flux;
 
 public interface DeckCardRepository extends R2dbcRepository<DeckCard, String>{
+    @Query("SELECT * FROM deck_card WHERE deck_id = $1")
+    Flux<DeckCard> findByDeckId(int id);
 }
