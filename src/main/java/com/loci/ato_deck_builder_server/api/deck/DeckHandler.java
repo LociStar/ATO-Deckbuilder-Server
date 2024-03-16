c924c139ee2244e44cc07dc8241dc01376660b99
LociStar <locistar.got@gmail.com>
2024-03-16 11:39:48 +0100

    implemented getCardDetails

--- src/main/java/com/loci/ato_deck_builder_server/api/deck/DeckHandler.java
+++ src/main/java/com/loci/ato_deck_builder_server/api/deck/DeckHandler.java
@@ -1,10 +1,14 @@
 package com.loci.ato_deck_builder_server.api.deck;
 
 import com.loci.ato_deck_builder_server.api.deck.objects.WebDeck;
+import com.loci.ato_deck_builder_server.database.objects.Card;
 import com.loci.ato_deck_builder_server.database.objects.Deck;
 import com.loci.ato_deck_builder_server.database.objects.DeckCard;
+import com.loci.ato_deck_builder_server.database.objects.WebCard;
+import com.loci.ato_deck_builder_server.database.repositories.CardRepository;
 import com.loci.ato_deck_builder_server.database.repositories.DeckCardRepository;
 import com.loci.ato_deck_builder_server.database.repositories.DeckRepository;
+import com.loci.ato_deck_builder_server.services.KeycloakService;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
@@ -17,16 +21,21 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class DeckHandler {
     private final DeckRepository deckRepository;
+    private final CardRepository cardRepository;
     private final DeckCardRepository deckCardRepository;
+    private final KeycloakService keycloakService;
 
-    public DeckHandler(DeckRepository deckRepository, DeckCardRepository deckCardRepository) {
+    public DeckHandler(DeckRepository deckRepository, CardRepository cardRepository, DeckCardRepository deckCardRepository) {
         this.deckRepository = deckRepository;
+        this.cardRepository = cardRepository;
         this.deckCardRepository = deckCardRepository;
+        this.keycloakService = new KeycloakService();
     }
 
     public Mono<ServerResponse> getDecks(ServerRequest request) {
@@ -53,5 +62,37 @@ public class DeckHandler {
                     });
                 }).flatMap(Flux::collectList)
                 .then(ServerResponse.ok().build());
+    }
+
+    public Mono<ServerResponse> getDeckDetails(ServerRequest serverRequest) {
+        int id = Integer.parseInt(serverRequest.pathVariable("id"));
+        Mono<Deck> deckMono = deckRepository.findById(id);
+
+        return deckMono.flatMap(this::createWebDeck)
+                .flatMap(webDeck -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(webDeck));
+    }
+
+    private Mono<WebDeck> createWebDeck(Deck deck) {
+        return getDeckCards(deck)
+                .collectList()
+                .flatMap(cards -> getUsername(deck)
+                        .map(username -> createWebDeck(deck, cards, username)));
+    }
+
+    private Flux<WebCard> getDeckCards(Deck deck) {
+        return deckCardRepository.findByDeckId(deck.getId())
+                .flatMap(deckCard -> Flux.range(0, deckCard.getAmount())
+                        .flatMap(i -> cardRepository.findWebCardById(deckCard.getCardId())));
+    }
+
+    private Mono<String> getUsername(Deck deck) {
+        return keycloakService.getUsername(deck.getUserId());
+    }
+
+    private WebDeck createWebDeck(Deck deck, List<WebCard> cards, String username) {
+        WebDeck webDeck = deck.toWebDeck();
+        webDeck.setCardList(cards);
+        webDeck.setUsername(username);
+        return webDeck;
     }
 }
