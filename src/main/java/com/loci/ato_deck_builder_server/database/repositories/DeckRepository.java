4cb46dbb753e52598fba213f6e5d2364c260ecad
LociStar <locistar.got@gmail.com>
2024-04-07 20:00:12 +0200

    changed queries

--- src/main/java/com/loci/ato_deck_builder_server/database/repositories/DeckRepository.java
+++ src/main/java/com/loci/ato_deck_builder_server/database/repositories/DeckRepository.java
@@ -4,17 +4,17 @@ import com.loci.ato_deck_builder_server.api.deck.objects.WebDeck;
 import com.loci.ato_deck_builder_server.database.objects.Deck;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.data.repository.query.Param;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface DeckRepository extends R2dbcRepository<Deck, Integer> {
     @Query("""
             SELECT * FROM deck
-            WHERE title ILIKE $1
-            ORDER BY title
+            WHERE title ILIKE $1 AND char_id LIKE $4
             LIMIT $2 OFFSET $3
             """)
-    Flux<Deck> findByTitleContaining(String name, int limit, long offset);
+    Flux<Deck> findByTitle(String name, int limit, long offset, String charId);
 
     @Query("""
             INSERT INTO deck (title, description, char_id, user_id)
@@ -43,4 +43,7 @@ public interface DeckRepository extends R2dbcRepository<Deck, Integer> {
 
     @Query("SELECT COUNT(*) FROM deck_likes WHERE deck_id = $1 AND user_id = $2")
     Mono<Integer> countLikes(int id, String userId);
+
+    @Query("SELECT COUNT(d) FROM Deck d WHERE d.title LIKE $1")
+    Mono<Integer> countByTitleContaining(String searchQuery);
 }
