d5f10784ea9df8579c4cc78a0e98883af8387645
LociStar <locistar.got@gmail.com>
2024-04-11 12:28:52 +0200

    added queries

--- src/main/java/com/loci/ato_deck_builder_server/database/repositories/DeckRepository.java
+++ src/main/java/com/loci/ato_deck_builder_server/database/repositories/DeckRepository.java
@@ -19,11 +19,43 @@ public interface DeckRepository extends R2dbcRepository<Deck, Integer> {
 
     @Query("""
             SELECT * FROM deck
+            WHERE title ILIKE $1 AND char_id LIKE $4 AND user_id = $5
+            ORDER BY likes DESC, title
+            LIMIT $2 OFFSET $3
+            """)
+    Flux<Deck> findByTitle_likes(String name, int limit, long offset, String charId, String userId);
+
+    @Query("""
+            SELECT * FROM deck
+            WHERE title ILIKE $1 AND char_id LIKE $4 AND user_id != $5
+            ORDER BY likes DESC, title
+            LIMIT $2 OFFSET $3
+            """)
+    Flux<Deck> findByTitle_likes_unowned(String name, int limit, long offset, String charId, String userId);
+
+    @Query("""
+            SELECT * FROM deck
             WHERE title ILIKE $1 AND char_id LIKE $4
             ORDER BY title
             LIMIT $2 OFFSET $3
             """)
     Flux<Deck> findByTitle_title(String name, int limit, long offset, String charId);
+
+    @Query("""
+            SELECT * FROM deck
+            WHERE title ILIKE $1 AND char_id LIKE $4 AND user_id = $5
+            ORDER BY title
+            LIMIT $2 OFFSET $3
+            """)
+    Flux<Deck> findByTitle_title(String name, int limit, long offset, String charId, String userId);
+
+    @Query("""
+            SELECT * FROM deck
+            WHERE title ILIKE $1 AND char_id LIKE $4 AND user_id != $5
+            ORDER BY title
+            LIMIT $2 OFFSET $3
+            """)
+    Flux<Deck> findByTitle_title_unowned(String name, int limit, long offset, String charId, String userId);
 
     @Query("""
             INSERT INTO deck (title, description, char_id, user_id)
