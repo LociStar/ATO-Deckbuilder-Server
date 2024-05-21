1bba1657a62f031a8be7830a712d0551800bb2ac
LociStar <locistar.got@gmail.com>
2024-05-21 16:53:41 +0200

    added query

--- src/main/java/com/loci/ato_deck_builder_server/database/repositories/CardDetailRepository.java
+++ src/main/java/com/loci/ato_deck_builder_server/database/repositories/CardDetailRepository.java
@@ -1,8 +1,12 @@
 package com.loci.ato_deck_builder_server.database.repositories;
 
 import com.loci.ato_deck_builder_server.database.objects.CardDetail;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
 
 public interface CardDetailRepository extends R2dbcRepository<CardDetail, String> {
 
+    @Query("SELECT sprite FROM card_detail WHERE card_id = $1")
+    Mono<String> findSpriteById(String id);
 }
