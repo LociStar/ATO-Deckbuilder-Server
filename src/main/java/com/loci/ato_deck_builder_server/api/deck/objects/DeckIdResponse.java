1e0b24af3f845255b98432b1352abc55dc0bd94a
LociStar <locistar.got@gmail.com>
2024-05-21 16:55:30 +0200

    init

---
+++ src/main/java/com/loci/ato_deck_builder_server/api/deck/objects/DeckIdResponse.java
@@ -0,0 +1,11 @@
+package com.loci.ato_deck_builder_server.api.deck.objects;
+
+import lombok.*;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeckIdResponse {
+    private int id;
+}
