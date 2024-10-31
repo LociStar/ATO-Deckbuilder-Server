1aaeaf1ab74b0daedd916383902c183badbe3db0
LociStar <locistar.got@gmail.com>
2024-10-31 18:22:10 +0100

    enabled caching

--- src/main/java/com/loci/ato_deck_builder_server/AtoDeckBuilderServerApplication.java
+++ src/main/java/com/loci/ato_deck_builder_server/AtoDeckBuilderServerApplication.java
@@ -2,7 +2,9 @@ package com.loci.ato_deck_builder_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class AtoDeckBuilderServerApplication {
 
