2b93f5ef341d1615fc39b6086cf693cce63fe751
LociStar <locistar.got@gmail.com>
2024-11-29 20:16:09 +0100

    changed object name

--- src/main/java/com/loci/ato_deck_builder_server/services/PerksService.java
+++ src/main/java/com/loci/ato_deck_builder_server/services/PerksService.java
@@ -3,8 +3,8 @@ package com.loci.ato_deck_builder_server.services;
 import com.loci.ato_deck_builder_server.api.perk.PagedPerks;
 import com.loci.ato_deck_builder_server.api.perk.WebPerk;
 import com.loci.ato_deck_builder_server.database.objects.PerkDetails;
-import com.loci.ato_deck_builder_server.database.objects.PerkNode;
 import com.loci.ato_deck_builder_server.database.objects.Perks;
+import com.loci.ato_deck_builder_server.dto.PerkNodeDTO;
 import org.springframework.core.io.buffer.DataBuffer;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -20,7 +20,7 @@ public interface PerksService {
 
     Mono<PerkDetails> getPerkDetails(String id);
 
-    Flux<PerkNode> getPerkNodes();
+    Flux<PerkNodeDTO> getPerkNodes();
 
     Mono<Void> deletePerk(String id);
 
