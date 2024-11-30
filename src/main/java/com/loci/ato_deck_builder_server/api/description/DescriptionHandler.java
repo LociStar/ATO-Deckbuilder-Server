439d860bdc7e6238ab7ff990c0fc21968bd41f89
LociStar <locistar.got@gmail.com>
2024-11-30 11:22:44 +0100

    added cache #minor

--- src/main/java/com/loci/ato_deck_builder_server/api/description/DescriptionHandler.java
+++ src/main/java/com/loci/ato_deck_builder_server/api/description/DescriptionHandler.java
@@ -2,10 +2,13 @@ package com.loci.ato_deck_builder_server.api.description;
 
 import com.loci.ato_deck_builder_server.services.DescriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Component
 public class DescriptionHandler {
@@ -19,7 +22,9 @@ public class DescriptionHandler {
 
     public Mono<ServerResponse> getDescription(ServerRequest request) {
         return descriptionService.getDescription(request.pathVariable("id"))
-                .flatMap(description -> ServerResponse.ok().bodyValue(description))
+                .flatMap(description -> ServerResponse.ok()
+                        .cacheControl(CacheControl.maxAge(Duration.ofDays(10)))
+                        .bodyValue(description))
                 .switchIfEmpty(ServerResponse.notFound().build());
     }
 }
--- src/main/java/com/loci/ato_deck_builder_server/services/DescriptionServiceImpl.java
+++ src/main/java/com/loci/ato_deck_builder_server/services/DescriptionServiceImpl.java
@@ -2,21 +2,31 @@ package com.loci.ato_deck_builder_server.services;
 
 import com.loci.ato_deck_builder_server.database.objects.Description;
 import com.loci.ato_deck_builder_server.database.repositories.DescriptionRepository;
+import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class DescriptionServiceImpl implements DescriptionService {
 
-    private final DescriptionRepository descriptionRepository;
+    private final AsyncLoadingCache<String, Description> descriptionCache;
 
     @Autowired
     public DescriptionServiceImpl(DescriptionRepository descriptionRepository) {
-        this.descriptionRepository = descriptionRepository;
+
+        this.descriptionCache = Caffeine.newBuilder()
+                .maximumSize(10000)
+                .expireAfterWrite(10, TimeUnit.DAYS)
+                .buildAsync((id, executor) -> descriptionRepository.findById(id).toFuture());
     }
 
     public Mono<Description> getDescription(String id) {
-        return descriptionRepository.findById(id);
+        CompletableFuture<Description> descriptionFuture = descriptionCache.get(id);
+        return Mono.fromFuture(descriptionFuture);
     }
 }
